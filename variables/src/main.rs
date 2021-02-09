16a5e72a24741d86a88522172ecb6cfa37e196dc
Santiago Narvaez <santiagonar1@gmail.com>
2021-02-09 14:25:05 +0100

    Add example of using _ as delimiter for numbers

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,7 +3,7 @@ const MAX_POINTS: u32 = 100_000;
 fn main() {
     let mut x = 5;
     println!("The value of x is: {}", x);
-    x = 6;
+    x = 6_000; // We can use _ as separator for clarity
     println!("The value of x is: {}", x);
     println!("The constant is: {}", MAX_POINTS);
 }
