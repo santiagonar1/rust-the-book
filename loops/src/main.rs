dd1e314739d87ea8b3e4cf40a367b8dd043300b8
Santiago Narvaez <santiagonar1@gmail.com>
2021-02-09 19:29:48 +0100

    Add for loop example

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -20,4 +20,15 @@ fn main() {
     }
 
     println!("LIFTOFF!!!");
+
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
+
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
 }
