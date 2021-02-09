29e648cd1d7b17915e9c0e74cedc8d9dd3ad4093
Santiago Narvaez <santiagonar1@gmail.com>
2021-02-09 17:41:15 +0100

    Add function that return value

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,7 +1,15 @@
 fn main() {
-    another_function(5);
+    let x = five();
+    another_function(x);
 }
 
 fn another_function(x: i32) {
     println!("The value of x is: {}", x);
+}
+
+fn five() -> i32 {
+    // In Rust the last expression is return implicitly. You can determine
+    // that is an expression by the fact that does not end with a semicolon.
+    // Other option is to use the return keyword.
+    5
 }
