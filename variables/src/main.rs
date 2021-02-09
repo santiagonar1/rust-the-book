33b164df61c55e5e8d416ff25452c904438b1070
Santiago Narvaez <santiagonar1@gmail.com>
2021-02-09 14:25:25 +0100

    Add example tuple

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -6,4 +6,10 @@ fn main() {
     x = 6_000; // We can use _ as separator for clarity
     println!("The value of x is: {}", x);
     println!("The constant is: {}", MAX_POINTS);
+
+    // Tuple declaration
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!("Tuple: {}, {}, {}", x, y, z);
+    println!("Tuple: {}, {}, {}", tup.0, tup.1, tup.2);
 }
