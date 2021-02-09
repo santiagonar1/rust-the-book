6fdcdc18b6d7b26b65f2381c1e5e9c264d38696f
Santiago Narvaez <santiagonar1@gmail.com>
2021-02-09 19:26:49 +0100

    Add while loop example

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -10,4 +10,14 @@ fn main() {
     };
 
     println!("The result is {}", result);
+
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{}!", number);
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
 }
