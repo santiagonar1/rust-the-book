14adf43766d3348e7605c1f77da24aab71f0d1f8
Santiago Narvaez <santiagonar1@gmail.com>
2021-02-10 10:52:53 +0100

    Add reference example for functions

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -12,12 +12,18 @@ fn main() {
 
     let s1 = gives_ownership(); // gives_ownership moves its return
                                 // value into s1
+    println!("Printing to avoid warning {}", s1);
 
     let s2 = String::from("hello"); // s2 comes into scope
 
     let s3 = takes_and_gives_back(s2); // s2 is moved into
                                        // takes_and_gives_back, which also
                                        // moves its return value into s3
+    println!("Printing to avoid warning {}", s3);
+
+    let s4 = String::from("hello");
+    let len = calculate_length(&s4);
+    println!("The length of '{}' is {}.", s4, len);
 } // Here, x goes out of scope, then s. But because s's value was moved, nothing
   // special happens. Also, s3 goes out of scope and is dropped. s2 goes out of
   // scope but was moved, so nothing happens. s1 goes out of scope and is dropped.
@@ -52,3 +58,9 @@ fn takes_and_gives_back(a_string: String) -> String {
 
     a_string // a_string is returned and moves out to the calling function
 }
+
+// s is a reference to a String
+fn calculate_length(s: &String) -> usize {
+    s.len()
+} // Here, s goes out of scope. But because it does not have ownership of what
+  // it refers to, nothing happens.
