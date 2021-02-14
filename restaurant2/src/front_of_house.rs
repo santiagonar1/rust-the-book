f0018683c5a61fd4284acb7322a2fc0d24a41ad7
Santiago Narvaez <santiagonar1@gmail.com>
2021-02-14 15:44:56 +0100

    Move hosting and serving to their own files

--- restaurant2/src/front_of_house.rs
+++ restaurant2/src/front_of_house.rs
@@ -1,13 +1,2 @@
-pub mod hosting {
-    pub fn add_to_waitlist() {}
-
-    fn seat_at_table() {}
-}
-
-mod serving {
-    fn take_order() {}
-
-    fn serve_order() {}
-
-    fn take_payment() {}
-}
+pub mod hosting;
+mod serving;
