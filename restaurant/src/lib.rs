ff109e66237514b05f796dfaf0f37a78f37b4b3b
Santiago Narvaez <santiagonar1@gmail.com>
2021-02-14 14:19:29 +0100

    Access functions from one module from other one
    Here we can see which modules must be pub to access a certain function.

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,6 +1,6 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
 
         fn seat_at_table() {}
     }
@@ -13,3 +13,24 @@ mod front_of_house {
         fn take_payment() {}
     }
 }
+
+pub fn eat_at_restaurant() {
+    // Absolute path. Since add_to_waitlist is defined on the
+    // same crate as eat_at_reastaurant, we can use the "crate"
+    // keyword.
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+}
+
+// Explanation:
+// In the absolute path, we start with crate, the root of our crate’s
+// module tree. Then the front_of_house module is defined in the crate
+// root. The front_of_house module isn’t public, but because the
+// eat_at_restaurant function is defined in the same module as
+// front_of_house (that is, eat_at_restaurant and front_of_house are siblings),
+// we can refer to front_of_house from eat_at_restaurant. Next is the hosting
+// module marked with pub. We can access the parent module of hosting,
+// so we can access hosting. Finally, the add_to_waitlist function is marked
+// with pub and we can access its parent module, so this function call works!
